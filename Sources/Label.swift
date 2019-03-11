7c437b6d3dc25983af5c84a3a8936aa27fb5374b
fumoboy007 <fumoboy007@me.com>
2019-03-11 15:53:11 -0700

    Set the accessibility role of `Label` to `.staticText`

--- Sources/Label.swift
+++ Sources/Label.swift
@@ -38,6 +38,8 @@ public class Label: AutoLayoutTextView {
    }
 
    private func commonInit() {
+      setAccessibilityRole(.staticText)
+
       isEditable = false
       drawsBackground = false
    }
